c8998a98ceb18a41dddfc0bb29c2893af14bb815
c-medeiros <claramedeiros11037@gmail.com>
2018-08-31 17:15:22 -0300

    ?!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import NewUser from './components/NewUser';
 
 class App extends Component {
   render() {
