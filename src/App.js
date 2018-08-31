f39a7c6dad123f63c093d0668d30f944d75268b3
c-medeiros <claramedeiros11037@gmail.com>
2018-08-31 17:26:32 -0300

    i guess??????

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import NewUser from './components/NewUser';
+import Label from './components/Label';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <NewUser />
+        <Label />
       </div>
     );
   }
