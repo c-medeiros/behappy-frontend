28c0ed8fb262cc9232f051e1023b622f08ef7108
c-medeiros <claramedeiros11037@gmail.com>
2018-09-03 15:55:04 -0300

    agora vai

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
 
 import Label from '../Label';
+import Input from '../Input';
 
 class NewUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: 'Minora'
+      },
+      validation: {
+        invalidName: false
+      }
+    };
+  }
+
+  updateUserName = (event) => {
+    let user = this.state.user;
+    user.name = event.target.value;
+    this.setState({
+      user: user
+    });
+  }
+
   render() {
     return (
       <div className="center">
         <form className="pure-form pure-form-stacked">
-          <Label htmlFor="name" text="Quem é você?" />
+          <Label 
+              htmlFor="name" 
+              text="Quem é você?" 
+              invalidValue={this.state.validation.invalidName} />
+          <Input 
+              id="name" 
+              placeholder="Digite seu nome" 
+              maxLength="40" 
+              readOnly={false}
+              invalidValue={this.state.validation.invalidName}
+              defaultValue={this.state.user.name}
+              onChange={this.updateUserName} />
         </form>
       </div>
     );
